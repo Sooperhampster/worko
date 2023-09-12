958f062882d84821d71f38019c6b5da23d91a00c
Zachary Davis <zacharydavis204@gmail.com>
2023-09-12 15:19:20 -0400

    Updated up to firebase

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,36 +1,115 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { loginPageForm } from './login.page.form';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/store/AppState';
 import { show, hide } from 'src/store/loading/loading.actions';
+import { recoverPassword, recoverPasswordSuccess, recoverPasswordFail, login, loginSuccess, loginFail } from 'src/store/login/login.actions';
+import { ToastController } from '@ionic/angular';
+import { LoginState } from 'src/store/login/LoginState';
+import { AuthService } from 'src/app/services/auth.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage implements OnInit, OnDestroy {
 
   form: FormGroup;
+  loginStateSubscription: Subscription;
   
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>) { }
+  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>, private toastController: ToastController, private authService: AuthService) { }
 
   ngOnInit() {
     this.form = new loginPageForm(this.formBuilder).createForm();
+
+    this.loginStateSubscription = this.store.select('login').subscribe(loginState => {
+      this.onIsRecoveredPassword(loginState);
+      this.onIsRecoveringPassword(loginState);
+
+      this.onIsLoggingIn(loginState);
+      this.onIsLoggedIn(loginState);
+
+      this.toggleLoading(loginState);
+      this.onError(loginState);
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.loginStateSubscription){
+      this.loginStateSubscription.unsubscribe();
+    }
+  }
+
+  private toggleLoading(loginState:LoginState){
+    if (loginState.isLoggingIn || loginState.isRecoveringPassword){
+      this.store.dispatch(show());
+    } else {
+      this.store.dispatch(hide());
+    }
+  }
+
+  private onIsLoggedIn(loginState: LoginState){
+    if (loginState.isLoggedIn){
+      this.router.navigate(['home']);
+    }
+  }
+
+  private onIsLoggingIn(loginState: LoginState){
+    if (loginState.isLoggingIn){
+      const email = this.form.get('email').value;
+      const password = this.form.get('password').value;
+      this.authService.login(email, password).subscribe(user => {
+        this.store.dispatch(loginSuccess({user}));
+      }, error => {
+        this.store.dispatch(loginFail({error}));
+      })
+    }
+  }
+
+  private async onError(LoginState: LoginState){
+    if (LoginState.error){
+      const toaster = await this.toastController.create({
+        position: "bottom",
+        message: LoginState.error.message,
+        color: "danger"
+      });
+      toaster.present();
+    }
+  }
+
+  private onIsRecoveringPassword(loginState: LoginState){
+    if (loginState.isRecoveringPassword){
+      this.authService.recoverEmailPassword(this.form.get('email').value).subscribe(() => {
+        this.store.dispatch(recoverPasswordSuccess());
+      }, error => {
+        this.store.dispatch(recoverPasswordFail({error}))
+      });
+    }
+  }
+
+  private async onIsRecoveredPassword(loginState: LoginState){
+    if (loginState.isRecoveredPassword){
+      const toaster = await this.toastController.create({
+        position: "bottom",
+        message: "Recovery email sent",
+        color: "secondary"
+      });
+      toaster.present();
+    }
   }
 
   forgotEmailPassword(){
-    this.store.dispatch(show());
-    setTimeout(() => {
-      this.store.dispatch(hide())
-    }, 3000)
+    this.store.dispatch(recoverPassword());
   }
 
   login(){
-    this.router.navigate(['home']);
+    this.store.dispatch(login());
   }
 
   register(){
